d227d48b06bbf34ace1a10f5bddcd5d3c00622f4
Esakul Islam Siam <123825715+digitpadweb@users.noreply.github.com>
2023-03-19 01:52:58 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          color01: '#FFC178',
+          color02: '#FFA343',
+          color03: '#FFD1A0',
+        },
+        maxWidth: {
+            'container': '1360px',
+        },
+        backgroundImage: {
+          'hero': "url('/images/banner.png')",
+        },
+        fontFamily: {
+            'sans': ['Open Sans'],
+          }
+
+    }
+  }
+}
